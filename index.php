e3acf5dd1396a79c8c706cbf274b507211a69c31
Maulana Ramadhan <58567048+maulanaramadhansiregar@users.noreply.github.com>
2019-12-06 01:58:57 +0700

    Add files via upload

---
+++ index.php
@@ -0,0 +1,26 @@
+<html>
+    <table>
+        <ul>Ferdinand</ul>
+            <li>maul</li>
+</table>
+
+<h2>
+<?php
+
+    echo "Hello";
+
+echo "<table>";
+    echo "<ul>";
+        echo "This Is Php";
+        echo "<li>";
+        echo "Baris ke 2";
+        echo "<li>";
+        echo "Baris ke 3";
+        echo "<li>";
+        echo "Baris Ke 4";
+        echo "</ul>";
+echo "</table>";
+
+?>
+    </h2>   
+</html>
